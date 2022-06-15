c0e15fbb5fabf52590b68f9c257c5dc1870e2698
yyh <1697149091@qq.com>
2022-06-15 16:42:03 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/yyh/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyh.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/yyh/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyh.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
