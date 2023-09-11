578e09c890a80c3928ae79e3df84d7d121253ea2
yaoyehan <1697149091@qq.com>
2023-09-11 23:53:38 +0800

    依赖bug修改完成

---
+++ wumao-contentRecommender/src/main/scala/com/pcy/content/Test.scala
@@ -0,0 +1,14 @@
+package com.pcy.content
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val s =
+      """
+        |select
+        |name
+        |age
+        |
+        |""".stripMargin
+    }
+
+  }
